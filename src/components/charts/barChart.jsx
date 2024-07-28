0e984d4b2e94e7abdf69c0ba4ef4bc047a8635d5
capsonchik <serega00790@mail.ru>
2024-07-28 21:48:04 +0300

    Возможность выбора и добавления второго параметра

--- src/components/charts/barChart.jsx
+++ src/components/charts/barChart.jsx
@@ -1,6 +1,9 @@
 import ReactECharts from "echarts-for-react";
+import {useSelector} from "react-redux";
+import {selectSecondParam} from "../../store/mainSelectors.js";
 
 export const BarChart = () => {
+  const secondParam = useSelector(selectSecondParam)
   const oneData = [
     {
       data: [120, 200, 150, 80, 70, 110, 130],
@@ -28,16 +31,7 @@ export const BarChart = () => {
     yAxis: {
       type: 'value'
     },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
-      },
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
-      }
-    ]
+    series: secondParam ? twoData : oneData
   };
 
   return (
--- src/pages/dataPage/DataPage.jsx
+++ src/pages/dataPage/DataPage.jsx
@@ -3,12 +3,15 @@ import {Header} from "../../components/header/Header.jsx";
 import {Button, SelectPicker} from "rsuite";
 import { FaChartLine } from "react-icons/fa";
 import { FaChartBar } from "react-icons/fa";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setCurrentGraph} from "../../store/mainSlice.js";
 import {BarChart} from "../../components/charts/barChart.jsx";
+import {selectSecondParam} from "../../store/mainSelectors.js";
 
 export const DataPage = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const secondParam = useSelector(selectSecondParam);
+
   const data = [
     'Eugenia',
     'Bryan',
@@ -25,6 +28,11 @@ export const DataPage = () => {
         <div className={styles.paramsBlock}>
           <span className={styles.paramsBlockTitle}>Настройка параметров</span>
           <SelectPicker data={data} placeholder={'Параметр 1'}/>
+          {secondParam ? (
+            <div className={styles.secondParam}>
+              <SelectPicker style={{width: '100%'}} data={data} placeholder={'Параметр 2'}/>
+            </div>
+          ) : null}
           <span className={styles.paramsBlockGraph}>График для вывода</span>
           <div className={styles.btnsGroop}>
             <Button onClick={() => dispatch(setCurrentGraph('line'))} className={styles.btn}>
