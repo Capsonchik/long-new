1272d7e8b075fb3c35b7438a1f1efeba5ba155d4
Khaustov.S <Khaustov.S@romir.ru>
2024-08-14 12:42:15 +0300

    Подключение реакции на кнопки

--- src/components/externalFactors/ExternalFactors.jsx
+++ src/components/externalFactors/ExternalFactors.jsx
@@ -3,8 +3,8 @@ import {Button} from "rsuite";
 import {useDispatch, useSelector} from "react-redux";
 import {selectCulture, selectLong, selectSocio} from "../../store/mainSelectors.js";
 import {useNavigate} from "react-router-dom";
-import {selectDataLoader} from "../../store/firstParamsSlice/firstParam.selectors.js";
-import {selectIsSecondParamDone} from "../../store/secondParamSlice/secondParamSelectors.js";
+import {selectDataLoader, selectFirstScaleType} from "../../store/firstParamsSlice/firstParam.selectors.js";
+import {selectIsSecondParamDone, selectSecondScaleType} from "../../store/secondParamSlice/secondParamSelectors.js";
 import {setFilterDrawerStatus} from "../../store/mainSlice.js";
 
 export const ExternalFactors = () => {
@@ -14,12 +14,22 @@ export const ExternalFactors = () => {
   const culture = useSelector(selectCulture);
   const dataLoader = useSelector(selectDataLoader);
   const isSecondParamDone = useSelector(selectIsSecondParamDone);
+  const firstScaleType = useSelector(selectFirstScaleType);
+  const secondScaleType = useSelector(selectSecondScaleType);
+
 
   const navigate = useNavigate()
 
   const handleNavigate = () => {
     navigate('/data')
   }
+
+  const isButtonEnabled = (firstScaleType, secondScaleType) => {
+    return (
+      (firstScaleType === 'категориальная' || firstScaleType === 'иерархичная') &&
+      (secondScaleType === 'категориальная' || secondScaleType === 'иерархичная')
+    );
+  };
 
   return (
     <div className={styles.externalContainer}>
@@ -29,12 +39,39 @@ export const ExternalFactors = () => {
           <Button onClick={handleNavigate} loading={dataLoader} disabled={!isSecondParamDone} color="orange"
                   appearance="primary"
                   className={styles.btn}>Данные</Button>
-          <Button onClick={handleNavigate} disabled={socio} color="orange" appearance="primary"
-                  className={styles.btn}>Хи-квадрат</Button>
-          <Button onClick={handleNavigate} disabled={culture} color="orange" appearance="primary"
-                  className={styles.btn}>Корреляция</Button>
-          <Button onClick={handleNavigate} disabled={culture} color="orange" appearance="primary"
-                  className={styles.btn}>Ф-тест Фишера</Button>
+          <Button
+            onClick={handleNavigate}
+            disabled={!(
+              (firstScaleType === 'категориальная' || firstScaleType === 'иерархичная') &&
+              (secondScaleType === 'категориальная' || secondScaleType === 'иерархичная')
+            )}
+            color="orange"
+            appearance="primary"
+            className={styles.btn}
+          >
+            Хи-квадрат
+          </Button>
+          <Button
+            onClick={handleNavigate}
+            disabled={!(firstScaleType === 'метрическая' && secondScaleType === 'метрическая')}
+            color="orange"
+            appearance="primary"
+            className={styles.btn}
+          >
+            Корреляция
+          </Button>
+          <Button
+            onClick={handleNavigate}
+            disabled={!(
+              (firstScaleType === 'метрическая' || firstScaleType === 'иерархичная') &&
+              (secondScaleType === 'категориальная' || secondScaleType === 'номинальная')
+            )}
+            color="orange"
+            appearance="primary"
+            className={styles.btn}
+          >
+            Ф-тест Фишера
+          </Button>
           <Button onClick={() => dispatch(setFilterDrawerStatus(true))} color="orange" appearance="primary"
                   className={styles.btn}>Панель фильтров</Button>
         </div>
